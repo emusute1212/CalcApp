447b19d7f5872e37256d58ebde6dbf81a0f13ced
宮西洋輔 <miyanishi1212@gmail.com>
2019-02-11 00:43:16 +0900

    feat(calc): ApplicationでDaggerをbuildさせるようにし、ApplicationComponentを整備した

--- app/src/main/java/com/example/yosuke/calculator/CalcApplication.kt
+++ app/src/main/java/com/example/yosuke/calculator/CalcApplication.kt
@@ -1,3 +1,11 @@
 package com.example.yosuke.calculator
 
-class CalcApplication
+import com.example.yosuke.calculator.di.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.support.DaggerApplication
+
+class CalcApplication : DaggerApplication() {
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerApplicationComponent.builder().app(this).build()
+    }
+}
--- app/src/main/java/com/example/yosuke/calculator/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/yosuke/calculator/di/ApplicationComponent.kt
@@ -4,10 +4,15 @@ import com.example.yosuke.calculator.CalcApplication
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class
+    ]
+)
 interface ApplicationComponent : AndroidInjector<CalcApplication> {
     @Component.Builder
     interface Builder {
