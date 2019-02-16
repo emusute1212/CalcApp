cd03bec326eb9228a1c36680cdb55cdd9da803c3
宮西洋輔 <miyanishi1212@gmail.com>
2019-02-17 01:30:28 +0900

    fix(calc): ActivityのセットにDataBindingを使用するようにした

--- app/src/main/java/com/example/yosuke/calculator/MainActivity.kt
+++ app/src/main/java/com/example/yosuke/calculator/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.yosuke.calculator
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.yosuke.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
