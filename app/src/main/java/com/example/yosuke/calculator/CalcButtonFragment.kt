99a243177ff5139607feeca7cb50a523d0573745
宮西洋輔 <miyanishi1212@gmail.com>
2019-03-10 01:24:35 +0900

    feat(calc): OperatorButtonAdapterをセットするようにした

--- app/src/main/java/com/example/yosuke/calculator/CalcButtonFragment.kt
+++ app/src/main/java/com/example/yosuke/calculator/CalcButtonFragment.kt
@@ -5,10 +5,12 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.yosuke.calculator.databinding.FragmentCalcButtonBinding
+import com.example.yosuke.calculator.view.OperatorButtonAdapter
 import com.example.yosuke.calculator.view.SpecialButtonAdapter
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_calc_button.view.*
@@ -19,7 +21,6 @@ class CalcButtonFragment : DaggerFragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     val viewModel by lazy { ViewModelProviders.of(requireActivity(), viewModelFactory).get(CalcViewModel::class.java) }
-    val adapter by lazy { SpecialButtonAdapter(viewModel) }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentCalcButtonBinding>(
@@ -29,16 +30,37 @@ class CalcButtonFragment : DaggerFragment() {
             false
         )
         binding.viewModel = viewModel
-        binding.root.special_button_recycler_view.apply {
-            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false).also {
-                layoutManager = it
-                adapter = this@CalcButtonFragment.adapter
-            }
-        }
-        viewModel.number.observe(this, Observer {
-            adapter.notifyDataSetChanged()
-        })
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //adapterのインスタンス作成
+        val specialButtonAdapter = SpecialButtonAdapter(viewModel)
+        val operatorButtonAdapter = OperatorButtonAdapter(viewModel)
+        //adapterのセットアップ
+        setupSpecialButtonAdapter(view.special_button_recycler_view, specialButtonAdapter)
+        setupOperatorButtonAdapter(view.operator_button_recycler_view, operatorButtonAdapter)
+
+        //C・ACボタンをセットするためのObserverを設定
+        viewModel.number.observe(this, Observer {
+            specialButtonAdapter.notifyDataSetChanged()
+        })
+    }
+
+    private fun setupSpecialButtonAdapter(recyclerView: RecyclerView, adapter: SpecialButtonAdapter) {
+        LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false).also {
+            recyclerView.layoutManager = it
+            recyclerView.adapter = adapter
+        }
+    }
+
+    private fun setupOperatorButtonAdapter(recyclerView: RecyclerView, adapter: OperatorButtonAdapter) {
+        LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false).also {
+            recyclerView.layoutManager = it
+            recyclerView.adapter = adapter
+        }
     }
 
     companion object {
