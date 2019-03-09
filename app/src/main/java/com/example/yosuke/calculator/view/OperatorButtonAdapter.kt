7721a91466f9c3cafa4d144c4bf34ca6aa873040
宮西洋輔 <miyanishi1212@gmail.com>
2019-03-10 01:24:05 +0900

    feat(calc): 四則演算用のボタンのAdapterクラスを作成

---
+++ app/src/main/java/com/example/yosuke/calculator/view/OperatorButtonAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.yosuke.calculator.view
+
+import android.databinding.DataBindingUtil
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.yosuke.calculator.CalcViewModel
+import com.example.yosuke.calculator.R
+import com.example.yosuke.calculator.view.buttons.Operators
+
+class OperatorButtonAdapter(
+    private val viewModel: CalcViewModel
+) : RecyclerView.Adapter<OperatorButtonAdapter.OperatorButtonViewHolder>() {
+
+    override fun getItemCount() = Operators.values().size
+
+    override fun onBindViewHolder(holder: OperatorButtonViewHolder, position: Int) {
+        if (holder.binding == null) return
+        holder.binding.viewModel = viewModel
+        holder.binding.buttonEntity = Operators.values()[position]
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OperatorButtonViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.button_view, parent, false)
+
+        return OperatorButtonViewHolder(view)
+    }
+
+    inner class OperatorButtonViewHolder(root: View) : RecyclerView.ViewHolder(root) {
+        val binding = DataBindingUtil.bind<com.example.yosuke.calculator.databinding.ButtonViewBinding>(root)
+    }
+}
