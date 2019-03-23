f54c4433b40985939d661a8eb3f6f2e6a6c74dae
宮西洋輔 <miyanishi1212@gmail.com>
2019-03-24 02:54:14 +0900

    refactor(calc): CalcUseCaseImplの改名

--- app/src/main/java/com/example/yosuke/calculator/di/modules/UseCaseModule.kt
+++ app/src/main/java/com/example/yosuke/calculator/di/modules/UseCaseModule.kt
@@ -1,12 +1,12 @@
 package com.example.yosuke.calculator.di.modules
 
 import com.example.yosuke.calculator.model.usecase.CalcUseCase
-import com.example.yosuke.calculator.model.usecase.CalcUsecaseImpl
+import com.example.yosuke.calculator.model.usecase.CalcUseCaseImpl
 import dagger.Module
 import dagger.Provides
 
 @Module
 class UseCaseModule {
     @Provides
-    fun provideCalcUseCase(): CalcUseCase = CalcUsecaseImpl()
+    fun provideCalcUseCase(): CalcUseCase = CalcUseCaseImpl()
 }
--- app/src/main/java/com/example/yosuke/calculator/model/usecase/CalcUseCaseImpl.kt
+++ app/src/main/java/com/example/yosuke/calculator/model/usecase/CalcUseCaseImpl.kt
@@ -2,7 +2,7 @@ package com.example.yosuke.calculator.model.usecase
 
 import com.example.yosuke.calculator.model.elements.Operators
 
-class CalcUsecaseImpl : CalcUseCase {
+class CalcUseCaseImpl : CalcUseCase {
     override fun calc(x: Long, operator: Operators, y: Long) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
