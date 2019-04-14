c1cbbec53038424889c574e03067b67bc926d2d3
宮西洋輔 <miyanishi1212@gmail.com>
2019-04-15 01:35:04 +0900

    feat: 計算履歴を表示させるようにした

--- app/src/main/java/com/example/yosuke/calculator/ext/TextViewExt.kt
+++ app/src/main/java/com/example/yosuke/calculator/ext/TextViewExt.kt
@@ -3,8 +3,15 @@ package com.example.yosuke.calculator.ext
 import android.databinding.BindingAdapter
 import android.support.annotation.StringRes
 import android.widget.TextView
+import com.example.yosuke.calculator.model.entity.CalcEntity
+import com.example.yosuke.calculator.model.entity.toStr
 
 @BindingAdapter("android:setTextByRes")
 fun TextView.setTextByRes(@StringRes stringRes: Int) {
     setText(stringRes)
+}
+
+@BindingAdapter("android:showCalcList")
+fun TextView.showCalcList(progress: List<CalcEntity>) {
+    text = progress.joinToString("") { it.number.toString() + it.operator.toStr() }
 }
