57910edcac3000c11ee75890c45486138aa27ae2
宮西洋輔 <miyanishi1212@gmail.com>
2019-05-28 00:34:03 +0900

    feat: assets内のフォントを読み込む拡張関数を追加

--- app/src/main/java/com/example/yosuke/calculator/ext/TextViewExt.kt
+++ app/src/main/java/com/example/yosuke/calculator/ext/TextViewExt.kt
@@ -1,12 +1,19 @@
 package com.example.yosuke.calculator.ext
 
 import android.databinding.BindingAdapter
-import android.support.annotation.StringRes
 import android.widget.TextView
 import com.example.yosuke.calculator.model.entity.CalcEntity
 import com.example.yosuke.calculator.model.entity.toStr
+import android.graphics.Typeface
+
+
 
 @BindingAdapter("android:showCalcList")
 fun TextView.showCalcList(progress: List<CalcEntity>) {
     text = progress.joinToString("") { it.number.toString() + it.operator.toStr() }
+}
+
+@BindingAdapter("android:setFont")
+fun TextView.setFont(path: String) {
+    typeface = Typeface.createFromAsset(context.assets, path)
 }
