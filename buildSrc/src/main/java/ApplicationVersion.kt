d30e1c6225d5c04ebf5d77509878230f089eaa63
YosukeMiyanishi <miyanishi1212@gmail.com>
2021-08-15 17:39:17 +0900

    chore: バージョンコードミスが発覚したので回収する

--- buildSrc/src/main/java/ApplicationVersion.kt
+++ buildSrc/src/main/java/ApplicationVersion.kt
@@ -4,6 +4,7 @@ internal data class ApplicationVersion(
     private val hotfix: Int,
     private val revision: Int
 ) {
-    val code = (major * 1_000_000) + (minor * 10_000) + (hotfix * 100) + revision
+    // バージョンコードミスにより、majorバージョンを+1するようにしている
+    val code = ((major + 1) * 1_000_000) + (minor * 10_000) + (hotfix * 100) + revision
     val name = "$major.$minor.$hotfix.$revision"
 }
