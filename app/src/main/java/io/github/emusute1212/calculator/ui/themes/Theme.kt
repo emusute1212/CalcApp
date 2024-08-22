be4ec39646f8462ccb5127db12521db1fe7c0dd8
emusute1212 <miyanishi1212@gmail.com>
2024-08-23 02:08:15 +0900

    chore: Create themes

---
+++ app/src/main/java/io/github/emusute1212/calculator/ui/themes/Theme.kt
@@ -0,0 +1,59 @@
+package io.github.emusute1212.calculator.ui.themes
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary = White800,
+    secondary = LightGreen700,
+    onSecondary = DarkGreen700,
+    tertiary = LightGreen800,
+    onTertiary = DarkGreen700,
+    surface = White700,
+    onSurface = Black500,
+    background = White900,
+    onBackground = White800,
+    outline = Black700,
+    outlineVariant = Gray900,
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Black900,
+    secondary = DarkGreen600,
+    onSecondary = DarkGreen800,
+    tertiary = LightGreen900,
+    onTertiary = DarkGreen900,
+    surface = Gray800,
+    onSurface = White600,
+    background = Black600,
+    onBackground = Black800,
+    outline = White900,
+    outlineVariant = White500,
+)
+
+@Composable
+fun CalculatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        // Material color is unused
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
