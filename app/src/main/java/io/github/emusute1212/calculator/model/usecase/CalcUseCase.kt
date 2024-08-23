24bff84d1bcd2692c07dc3600bf21368c88d2220
emusute1212 <miyanishi1212@gmail.com>
2024-08-24 02:32:13 +0900

    chore: Fix builds

--- app/src/main/java/io/github/emusute1212/calculator/model/usecase/CalcUseCase.kt
+++
@@ -1,8 +0,0 @@
-package io.github.emusute1212.calculator.model.usecase
-
-import io.github.emusute1212.calculator.model.entity.Controller
-import java.math.BigDecimal
-
-interface CalcUseCase {
-    fun calc(x: BigDecimal, operator: Controller.Operators, y: BigDecimal): BigDecimal
-}
