5a1ed677acb62d9f7eed53549b9540669f517f81
emusute1212 <miyanishi1212@gmail.com>
2024-08-30 23:52:11 +0900

    feat: Impl oss license view

--- app/src/main/java/io/github/emusute1212/calculator/ui/main/CalculatorScreen.kt
+++ app/src/main/java/io/github/emusute1212/calculator/ui/main/CalculatorScreen.kt
@@ -1,9 +1,12 @@
 package io.github.emusute1212.calculator.ui.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,55 +14,118 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import io.github.emusute1212.calculator.R
 import io.github.emusute1212.calculator.model.entity.CalcEntity
 import io.github.emusute1212.calculator.model.entity.Controller
 import io.github.emusute1212.calculator.ui.component.AutoSizeText
 import io.github.emusute1212.calculator.ui.component.AutoTextAlign
+import io.github.emusute1212.calculator.ui.component.CalculatorDrawerContent
+import io.github.emusute1212.calculator.ui.component.DrawerContent
 import io.github.emusute1212.calculator.ui.main.section.numbers.NumberButtonSection
 import io.github.emusute1212.calculator.ui.main.section.numbers.NumberButtonSectionUiState
 import io.github.emusute1212.calculator.ui.main.section.operators.OperatorButtonSection
 import io.github.emusute1212.calculator.ui.main.section.operators.OperatorButtonSectionUiState
 import io.github.emusute1212.calculator.ui.main.section.specials.SpecialsButtonSection
 import io.github.emusute1212.calculator.ui.main.section.specials.SpecialsButtonSectionUiState
+import io.github.emusute1212.calculator.ui.menus.OssLicenseScreenRoute
 import io.github.emusute1212.calculator.ui.themes.CalculatorTheme
+import kotlinx.coroutines.launch
 import java.math.BigDecimal
 
+const val CalculatorScreenRoute = "calculator_screen"
+fun NavGraphBuilder.calculatorScreen(
+    navController: NavController,
+) {
+    composable(CalculatorScreenRoute) {
+        CalculatorScreenHost(
+            navController = navController
+        )
+    }
+}
+
 @Composable
-fun CalculatorScreenHost(
+private fun CalculatorScreenHost(
     viewModel: CalculatorViewModel = hiltViewModel(),
+    navController: NavController,
 ) {
     val state = rememberCalculatorScreenUiState(
         viewModel = viewModel
     )
-    CalculatorScreen(
-        state = state,
+
+    val drawerState = rememberDrawerState(
+        initialValue = DrawerValue.Closed,
     )
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            CalculatorDrawerContent(
+                onClickContent = {
+                    when (it) {
+                        DrawerContent.LICENSE -> {
+                            navController.navigate(
+                                route = OssLicenseScreenRoute
+                            )
+                            scope.launch {
+                                drawerState.close()
+                            }
+                        }
+                    }
+                }
+            )
+        },
+    ) {
+        Scaffold(
+            topBar = {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_menu_black_24dp),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(
+                            all = 16.dp,
+                        )
+                        .clickable {
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        },
+                )
+            },
+        ) { padding ->
+            CalculatorScreen(
+                state = state,
+                paddingValues = padding,
+            )
+        }
+    }
 }
 
 @Composable
 private fun CalculatorScreen(
     state: CalculatorScreenUiState,
+    paddingValues: PaddingValues,
 ) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(paddingValues)
             .background(color = MaterialTheme.colorScheme.background),
         verticalArrangement = Arrangement.Bottom,
     ) {
@@ -183,7 +249,8 @@ fun PreviewCalculatorScreen() {
                     canAllClear = true,
                     onAction = {}
                 )
-            )
+            ),
+            paddingValues = PaddingValues(0.dp),
         )
     }
 }
---
+++ app/src/main/java/io/github/emusute1212/calculator/ui/menus/OssLicenseScreen.kt
@@ -0,0 +1,85 @@
+package io.github.emusute1212.calculator.ui.menus
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
+import io.github.emusute1212.calculator.R
+import io.github.emusute1212.calculator.ui.themes.OssLicenseTheme
+
+const val OssLicenseScreenRoute = "oss_license_screen"
+fun NavGraphBuilder.ossLicenseScreen(
+    navController: NavController,
+) {
+    composable(OssLicenseScreenRoute) {
+        OssLicenseTheme {
+            OssLicenseScreen(
+                navController = navController,
+            )
+        }
+    }
+}
+
+@Composable
+private fun OssLicenseScreen(
+    navController: NavController,
+) {
+    Scaffold(
+        topBar = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .background(color = MaterialTheme.colorScheme.background),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Image(
+                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clickable {
+                                navController.popBackStack()
+                            },
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.open_source_license),
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                }
+                HorizontalDivider()
+            }
+        }
+    ) { padding ->
+        LibrariesContainer(
+            Modifier
+                .fillMaxSize()
+                .padding(padding)
+        )
+    }
+}
