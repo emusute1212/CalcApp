0522c18fe2fc3706474fbe2105aa5feb7020d75e
emusute1212 <miyanishi1212@gmail.com>
2026-09-10 13:57:41 +0900

    fix: adapt calculator screens to modern Android layouts

--- app/src/main/java/io/github/emusute1212/calculator/ui/MainActivity.kt
+++ app/src/main/java/io/github/emusute1212/calculator/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.emusute1212.calculator.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.emusute1212.calculator.ui.themes.CalculatorTheme
 
@@ -10,6 +11,7 @@ import io.github.emusute1212.calculator.ui.themes.CalculatorTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             CalculatorTheme {
